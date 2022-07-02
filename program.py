4e5a67b2c1ed7a8c07cc8ca91b66356594c6392d
AdamOsses <adamosses@users.noreply.github.com>
2022-07-02 16:18:28 +0200

    Poprawka petli: z while na if-y

--- program.py
+++ program.py
@@ -1,19 +1,18 @@
-# Adam61398 - Wybor regionu: USA/Europa
+# Adam61398 - Wybor regionu: USA/Europa 
 minimalny_wiek = 0
 print(40*'*')
 print('WITAJ NA STRONIE SKLEPU "MALY ALHOHOLIK"\n')
-while True:
-    print('Wybierz region:\n\t1. USA\n\t2. Europa')
-    region = str(input())
-    if region.lower() == 'USA' or region == '1' or region == '1.':
-        print('Wybrany region: USA (dorosly od 21 lat)')
-        minimalny_wiek = 21
-        break
-    if region.lower() == 'Europa' or region == '2' or region == '2.':
-        print('Wybrany region: Europa (dorosly od 18 lat)')
-        minimalny_wiek = 21
-        break
-    print('\nBledny region.')
+print('Wybierz region:\n\t1. USA\n\t2. Europa')
+region = str(input())
+if region.lower() == 'usa' or region == '1' or region == '1.':
+    print('Wybrany region: USA (dorosly od 21 lat)')
+    minimalny_wiek = 21
+elif region.lower() == 'europa' or region == '2' or region == '2.':
+    print('Wybrany region: Europa (dorosly od 18 lat)')
+    minimalny_wiek = 18
+else:
+    print('\nNiepoprawny region. Nie mozemy Cie obsluzyc.')
+    exit()
 print(40*'*')
 # Adam61398 - Koniec: wybor regionu: USA/Europa
 # Zmienna "minimalny_wiek" podaje minimalny wiek od ktorego mozna nabyc alkohol
@@ -23,7 +22,7 @@ wiek = input("Podaj wiek użytkownika: ")
 if wiek.isdigit() == False:
     exit("Wiek mosi być podany jako liczba")
 wiek = int(wiek)
-if wiek >= 18 and wiek <= 50:
+if wiek >= minimalny_wiek and wiek <= 50:
     print("Witaj w naszym sklepie z alkoholem")
 elif wiek > 50:
     print("Witaj w naszym sklepie z alkoholem")
